15da5c347cf65448f0bddc95e01e020a8b3cfcea
Andrey Sakharov <a.sakharov@biletix.ru>
2020-07-01 16:39:47 +0300

    MongooseModule was added

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,26 @@
 import { Module, OnApplicationShutdown } from '@nestjs/common';
 import { configModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
    imports: [
       configModule,
+      MongooseModule.forRootAsync({
+         useFactory: async (configService: ConfigService) => {
+            const dbHost = configService.get('DB_HOST');
+            const dbPort = configService.get('DB_PORT');
+            const dbUser = configService.get('DB_USER');
+            const dbPass = configService.get('DB_PASS');
+            const dbName = configService.get('DB_NAME');
+
+            return {
+               uri: `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`,
+            };
+         },
+         inject: [ConfigService],
+      }),
       TodoModule,
    ],
 })
